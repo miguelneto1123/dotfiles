cc9c68505f0215e4889423bf20b2675d29c8f269
Miguel Neto <11512605+miguelneto1123@users.noreply.github.com>
2021-05-13 01:30:54 -0300

    Added logger helper to print messages on install scripts

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,23 +4,8 @@ cd "$(dirname "$0")"
 REPO_ROOT="$(pwd -P)"
 DOTFILES_ROOT="$REPO_ROOT/dotfiles"
 
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
-}
+# Get logging functions
+source ./logger.sh
 
 setup_gitconfig () {
   if ! [ -f "$DOTFILES_ROOT/.gitconfig" ]
